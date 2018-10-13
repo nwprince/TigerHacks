cf1c5cadb5b6e1c2703bcff812d510d4f2154077
root <root@DESKTOP-7ANKV1C.localdomain>
2018-10-13 18:09:23 -0500

    go danny phantom go

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppBar from './components/AppBar';
 
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, View, Alert } from 'react-native';
 import Constants from './components/Constants';
 
 interface IAppState {
@@ -14,23 +14,61 @@ export default class App extends React.Component<{}, IAppState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      isLoadingSearch: false
+      isLoadingSearch: true
     };
+
+    this.fetchInitial(Constants.InitialTopics);
   }
 
   handleSearchPress = (text: string) => {
     text = text;
     console.log('success');
     this.setState({ isLoadingSearch: true });
+    this.fetchFromQuery(text);
   }
 
   fetchFromQuery(query: string) {
     fetch(Constants.AppUrl + '?' + query + '=10').then(response => {
       return response.json();
-    }).then(response => {
+    }).then(this.checkStatus).then(response => {
       this.queryData = response;
       this.setState({isLoadingSearch: false});
+    }).catch(e => {
+      console.log(e);
+      Alert.alert('Error', 'Server is offline');
     });
+  }
+
+  fetchInitial(topics: string[]) {
+    let queryString: string = undefined;
+
+    queryString += Constants.AppUrl;
+    for (let index = 0; index < topics.length; index++) {
+      queryString += '?';
+      queryString += topics[index];
+      queryString += '=3&';
+    }
+    console.log(queryString);
+    queryString = queryString.slice(0, queryString.length - 1);
+    console.log(queryString);
+    fetch(queryString).then(this.checkStatus)
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      this.queryData = data;
+      this.setState({isLoadingSearch: false});
+    }).catch(e => {
+      console.log(e);
+      Alert.alert('Error', 'Server is offline');
+    });
+  }
+
+  checkStatus(response: Response) {
+    if (response.ok) {
+      return response;
+    } else {
+      throw response;
+    }
   }
 
   render() {
--- src/components/Constants.tsx
+++ src/components/Constants.tsx
@@ -4,5 +4,6 @@ export default class Constants {
   static Width: number = Dimensions.get('window').width;
   static Height: number = Dimensions.get('window').height;
   static AppName: string = 'soberGatez';
-  static AppUrl: string = '';
+  static AppUrl: string = 'https://dd7d530b.ngrok.io/api/query';
+  static InitialTopics: string[] = ['Machine Learning', 'Security', 'Python', 'Music', 'Media'];
 }
